4c80272ada156e85f0f636ebd0ae60f3f3f23609
Rbavishi <rohan.bavishi95@gmail.com>
2015-05-12 01:35:31 +0530

    Converted file passed to iCBMC

--- main_script.sh
+++ main_script.sh
@@ -8,7 +8,6 @@ function icbmc () {
 function generate_trace () {
 	
 	##### GOTO Programs #####
-	touch converted.c
 	rm -rf trace-debug.txt
 	/home/rbavishi/CBMC\ -\ Bug\ Clustering\ Project/cbmc-5.0/src/goto-cc/goto-cc "$1" -o intermediate_program.gb
 	/home/rbavishi/CBMC\ -\ Bug\ Clustering\ Project/cbmc-5.0/src/goto-instrument/goto-instrument intermediate_program.gb --dump-c > generate_trace_converted.c
@@ -16,10 +15,10 @@ function generate_trace () {
 	/home/rbavishi/CBMC\ -\ Bug\ Clustering\ Project/cbmc-5.0/src/goto-instrument/goto-instrument intermediate_program.gb --list-symbols | sed  "/^__CPROVER.*$/d" | sed "/^_start.*$/d" > generate_trace_symbols.txt
 
 	##### Generate Counterexample #####
-	icbmc "$@"
+	icbmc generate_trace_converted.c "${@:2}"
 	if [ -a "trace-debug.txt" ]; then
+		echo "Okay"
 	else
-		echo "Nothing to do."
 		rm -rf intermediate_program.gb "*symbols.txt" "*converted.c"
 	fi
 
