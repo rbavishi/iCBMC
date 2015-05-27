6ce79375d89b521292ed2eff70f7f8422a7569ce
Rbavishi <rohan.bavishi95@gmail.com>
2015-05-27 22:52:04 +0530

    Creating new classes for holding actual instructions produced during symex

---
+++ cbmc-5.0/src/cbmc/icbmc_goto_trace.cpp
@@ -0,0 +1,11 @@
+#include "icbmc_goto_trace.h"
+
+int icbmc_goto_tracet::trial_func(
+  int a,
+  int b) 
+{
+  int c = a + b;
+  return c;
+}
+
+
---
+++ cbmc-5.0/src/cbmc/icbmc_goto_trace.h
@@ -0,0 +1,9 @@
+#include <goto-programs/goto_program.h>
+
+class icbmc_goto_tracet
+{
+public:
+  int trial1;
+  int trial2;
+  int trial_func(int a,int b);
+};
